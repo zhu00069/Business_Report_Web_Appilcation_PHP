a917f174742bf60c567110586c81a1ddd147b30b
zhu00069 <35508324+zhu00069@users.noreply.github.com>
2019-04-09 12:05:39 -0400

    Update db.php

--- db.php
+++ db.php
@@ -1,11 +1,12 @@
 <?php 
 
+//prepare parameters to connect dadabase
 $hostname = "localhost";
 $username = "root";
 $password = "yA621838";
 $dbname   = "lamter";
 
-
+//mysql database connection
 $connection = mysqli_connect($hostname,$username,$password, $dbname);
 
 
